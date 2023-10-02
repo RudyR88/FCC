4891477fd116c3bf3eac4f574cd3934d22b00a5f
Rudy Rivera <combustingkarma@gmail.com>
2023-10-02 08:15:12 -0500

    Serve an HTML file
    Challenge completed

--- back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
+++ back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
 
+
 // Challenge #1
 // console.log("Hello World");
 
+// Challenge #2
+// app.get('/', (req, res) => {
+//     res.send('Hello Express');
+// });
+
 app.get('/', (req, res) => {
-    res.send('Hello Express');
-});
+    res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
