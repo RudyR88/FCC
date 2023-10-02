4348b35ac912e65b591de69d2274527f60bf32ef
Rudy Rivera <combustingkarma@gmail.com>
2023-10-02 09:04:56 -0500

    Serve JSON on a specific route
    Challenge completed

--- back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
+++ back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
@@ -11,12 +11,16 @@ let app = express();
 // });
 
 // Challenge #3
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-    // Challenge #4
-    app.use('/public', express.static(__dirname + '/public'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/views/index.html');
+//     // Challenge #4
+//     app.use('/public', express.static(__dirname + '/public'));
+// });
 
+// Challenge #5
+app.get('/json', (req, res) => {
+    res.json({message: "Hello json"});
+})
 
 
 
