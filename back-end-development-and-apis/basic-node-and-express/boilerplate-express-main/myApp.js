b97063299f21c375d6a373edefed119c99211ede
Rudy Rivera <combustingkarma@gmail.com>
2023-10-04 00:16:07 -0500

    Get route parameter input form client
    Challenge completed

--- back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
+++ back-end-development-and-apis/basic-node-and-express/boilerplate-express-main/myApp.js
@@ -39,11 +39,16 @@ let app = express();
 // });
 
 // Challenge #8
-app.get('/now', (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-}, (req, res) => {
-    res.json({time: req.time});
+// app.get('/now', (req, res, next) => {
+//     req.time = new Date().toString();
+//     next();
+// }, (req, res) => {
+//     res.json({time: req.time});
+// });
+
+// Challenge #9
+app.get('/:word/echo', (req, res) => {
+    res.json({echo: req.params.word});
 });
 
 
