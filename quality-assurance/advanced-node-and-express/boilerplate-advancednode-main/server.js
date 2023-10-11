2ec1df1fe1d8b59a629b1adff9cff585ed2ace42
Rudy Rivera <combustingkarma@gmail.com>
2023-10-11 06:21:48 -0500

    Registration of new users
    Challenge completed

--- quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/server.js
+++ quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/server.js
@@ -36,7 +36,8 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -51,6 +52,30 @@ app.route('/profile').get(ensureAuthenticated, (req, res) => {
 app.route('/logout').get((req, res) => {
     req.logout();
     res.redirect('/');
+  });
+
+app.route('/register').post((req, res, next) => {
+  myDataBase.findOne({username: req.body.username}, (err, user) => {
+    if (err){
+      next(err);
+    } else if(user) {
+      res.redirect('/');
+    } else {
+      myDataBase.insertOne({
+        username: req.body.username,
+        password: req.body.password
+      }, (err, doc) => {
+        if(err) {
+          res.redirect('/');
+        } else {
+          next(null, doc.ops[0]);
+        }
+      })
+    }
+  })
+}, passport.authenticate('local', {failureRedirect: '/'}),
+  (req, res, next) => {
+    res.redirect('/profile');
   });
 
 app.use((req, res, next) => {
