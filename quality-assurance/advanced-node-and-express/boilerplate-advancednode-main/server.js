21760ff2a04477af86cb87bc0ec79a78aa546f6f
Rudy Rivera <combustingkarma@gmail.com>
2023-10-11 05:19:01 -0500

    Set up passport
    Challenge completed

--- quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/server.js
+++ quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -16,6 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 app.route('/').get((req, res) => {
   res.render('index', {title: 'Hello', message: 'Please log in'});
 });
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+passport.initialize();
+passport.session();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
