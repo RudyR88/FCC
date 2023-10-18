42e434bd8d86289b5d86c659d836ed588fd2ffea
Rudy Rivera <combustingkarma@gmail.com>
2023-10-17 19:36:16 -0500

    Announce new users
    Challenge completed

--- quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/public/client.js
+++ quality-assurance/advanced-node-and-express/boilerplate-advancednode-main/public/client.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
   /* global io*/
   let socket = io();
 
-  socket.on('user count', data => {
-    console.log(data);
+  socket.on('user', data => {
+    $('#num-users').text(data.currentUsers + ' users online');
+    let message = data.username + (data.conencted ? ' has joined the chat' : ' has left the chat');
+    $('#messages').appened($('<li>').html('<b>' + message + '</b>'));
   });
 
   // Form submittion with new message in field with id 'm'
